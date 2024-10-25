1f218fb83743d5968d139f53bfe1097a8b87f812
Jennifer Fox <jenniferelizfox@gmail.com>
2024-10-25 12:21:16 +0100

    Next.js template added.

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className='relative bg-black'>
+      <div>
+        <h1>
+          My Portfolio!
+        </h1>
+      </div>
+    </main>
+  );
+}

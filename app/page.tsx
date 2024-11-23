42d43c2d62c381fda3c96b5600100a5e6ee6fcd6
Jennifer Fox <jenniferelizfox@gmail.com>
2024-11-23 10:06:09 +0000

    cleaning up template and gitignore. Removing unnecessary files from next.js template

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Hero from '@/components/Hero'
+
+export default function Home() {
+  return (
+    <main className='relative bg-black flex
+    justify-center items-center flex-col overflow-hidden
+    mx-auto sm:px-10 px-5'>
+      <div className='max-w-7x w-full'>
+        <Hero />
+      </div>
+    </main>
+  );
+}
